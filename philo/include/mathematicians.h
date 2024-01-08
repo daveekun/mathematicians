bee7e4e16ba7aa9abeae003b74b0cf741bdc3045
David Horvath <dhorvath@c1r5p5.hive.fi>
2024-01-08 11:39:16 +0200

    basic thread

---
+++ philo/include/mathematicians.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mathematicians.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhorvath <dhorvath@hive.student.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 11:28:13 by dhorvath          #+#    #+#             */
+/*   Updated: 2024/01/08 11:29:50 by dhorvath         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MATHEMATICIANS_H
+# define MATHEMATICIANS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+#endif
